a3b5b18e56dffe488c387c23346f499685534363
SSAFY <kys980130@naver.com>
2022-03-21 23:56:46 +0900

    add 0321

--- BOJ/1655_가운데를_말해요/s1.py
+++ BOJ/1655_가운데를_말해요/s1.py
@@ -1,13 +1,22 @@
 import sys
+import heapq
 sys.stdin = open('input.txt')
 
 n = int(input())
-temp = []
+left_heap, right_heap = [], []
 for i in range(n):
-    temp.append(int(input()))
-    temp.sort()
-    length = len(temp)
-    if length % 2 == 0:
-        print(temp[length-1])
+    num = int(sys.stdin.readline())
+
+    if len(left_heap) == len(right_heap):
+        heapq.heappush(left_heap, -num)
     else:
-        print(temp[length])
+        heapq.heappush(right_heap, num)
+    
+    if right_heap and right_heap[0] < -left_heap[0]:
+        left = heapq.heappop(left_heap)
+        right = heapq.heappop(right_heap)
+
+        heapq.heappush(left_heap, -right)
+        heapq.heappush(right_heap, -left)
+    
+    print(-left_heap[0])
