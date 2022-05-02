2205cf5e6918fcb4e575a5529358f15b959dff8c
kys980130 <kys980130@naver.com>
2022-05-02 23:51:44 +0900

    Add 0502

--- BOJ/12865_평범한_배낭/s1.py
+++ BOJ/12865_평범한_배낭/s1.py
@@ -3,8 +3,8 @@ sys.stdin = open('input.txt')
 
 n, max_weight = map(int, input().split())
 temp = [0] * (max_weight + 1)
+lst = []
 for i in range(n):
     w, v = map(int, input().split())
-    for j in range(max_weight, w-1, -1):
-        temp[j] = max(temp[j], temp[j-w]+v)
-print(temp[max_weight])
+    lst.append([w, v])
+lst.sort()
